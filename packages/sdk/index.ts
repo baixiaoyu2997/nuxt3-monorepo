add04ac107d6c583af75fb6bc2e690aecb3589cb
L.Rain <bxy2997@sina.com>
2024-01-26 18:02:48 +0800

    feat: add sdk

---
+++ packages/sdk/index.ts
@@ -0,0 +1,3 @@
+export function useTest(params) {
+  return 1;
+}
---
+++ packages/sdk/vite.config.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // base: "./",
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./"),
+  //   },
+  // },
+  build: {
+    // target: "esnext",
+    lib: {
+      entry: "./index.ts",
+      formats: ["es"],
+    },
+    // minify: false,
+    // sourcemap: true,
+    // rollupOptions: {
+    //   treeshake: false,
+    //   output: {
+    //     dir: "dist",
+    //     format: "es",
+    //     exports: "named",
+    //     entryFileNames: "[name].mjs",
+    //     preserveModules: true, // Keep directory structure and files
+    //     // preserveModulesRoot: 'src',
+    //   },
+    // },
+  },
+});
